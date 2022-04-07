d5fc42e56cda04b3962424eee7685619b917b8f0
robertolatini31 <robertolatini31@gmail.com>
2022-04-07 15:01:48 +0200

    added bonus 2

--- Bonus_2/assets/js/main.js
+++ Bonus_2/assets/js/main.js
@@ -32,49 +32,35 @@ const team = [
     },
   ];
 
-//   MILESTONE 1:
-// stampare su console le informazioni di nome, ruolo e la stringa della foto
-
-// creo un ciclo che scorre l'array di oggetti
-for (let i = 0; i < team.length; i++) {
-    const element = team[i]; // a ogni iterazione l'oggetto in posizione i a una variabile element
-    for (let key in element) { // ciclo nell'oggetto ogni chiave
-        console.log(element[key]); // loggo il contenuto dell'oggetto per ogni chiave
-    }
-}
-
-// MILESTONE 2:
-// stampare le stesse informazioni su DOM sottoforma di stringhe
-
-// collego il punto della dom dove voglio inserire le stringhe
-const stringsPositionElement = document.querySelector('.container_strings');
-// copio il ciclo dei log e aggiongo la stampa a schermo
-for (let i = 0; i < team.length; i++) {
-    const element = team[i]; // a ogni iterazione l'oggetto in posizione i a una variabile element
-    let stringToPrint = []; // inizializoz una stringa vuota
-    for (let key in element) { // ciclo nell'oggetto ogni chiave
-       stringToPrint.push(element[key]);//aggiungo alla stringa tutti i valori delle parole chiave dell'oggetto
-    }
-    stringsPositionElement.innerHTML += `<p>${stringToPrint}</p>`; // mando a schermo il contenuto dell'oggetto in forma di tag <p> così è leggibile
-}
 
 
 // BONUS 1:
 // trasformare la stringa foto in una immagine effettiva
 
 //come prima collego il punto della dom dove stampare
-const stringsImgPositionElement = document.querySelector('.container_img_strings');
-// copio il ciclo precedente e modifico per inserire le img
+const cardPositionElement = document.querySelector('main .row');
+// copio il ciclo precedente e modifico per inserire le card
 for (let i = 0; i < team.length; i++) {
     const element = team[i]; // a ogni iterazione l'oggetto in posizione i a una variabile element
-    let stringToPrint = []; // inizializoz una stringa vuota
+    let elementsToPrint = []; // inizializoz una stringa vuota
     for (let key in element) { // ciclo nell'oggetto ogni chiave
-       stringToPrint.push(element[key]);//aggiungo alla stringa tutti i valori delle parole chiave dell'oggetto
+       elementsToPrint.push(element[key]);//aggiungo alla stringa tutti i valori delle parole chiave dell'oggetto
     }
     // stampo i primi due valori come prima mentre per le img inserisco il valore all'interno di un tag img con il percorso file già inserito
-    stringsImgPositionElement.innerHTML += `<p>${stringToPrint[0]}, ${stringToPrint[1]}, <img src="./assets/img/${stringToPrint[2]}"></p>`; // mando a schermo il contenuto dell'oggetto in forma di tag <p> così è leggibile
-}
-
+    cardPositionElement.innerHTML += `
+    <div class="col d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./assets/img/${elementsToPrint[2]}" class="card-img-top">
+                        <div class="card-body text-center">
+                            <h5>${elementsToPrint[0]}</h5>
+                            <p class="card-text">${elementsToPrint[1]}</p>
+                        </div>
+                    </div>
+                    <!-- /.card  -->
+                </div>
+                <!-- /.col -->
+    `
+  }
 
 // BONUS 2:
 // organizzare i singoli membri in card/schede
