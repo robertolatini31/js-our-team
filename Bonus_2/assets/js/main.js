5ed5b76fab0ed28f11c41a58825a28ae6d0c66a4
robertolatini31 <robertolatini31@gmail.com>
2022-04-07 15:56:23 +0200

    added form

--- Bonus_2/assets/js/main.js
+++ Bonus_2/assets/js/main.js
@@ -48,18 +48,46 @@ for (let i = 0; i < team.length; i++) {
        elementsToPrint.push(element[key]);//aggiungo alla stringa tutti i valori delle parole chiave dell'oggetto
     }
     // stampo i primi due valori come prima mentre per le img inserisco il valore all'interno di un tag img con il percorso file già inserito
-    cardPositionElement.innerHTML += `
-    <div class="col d-flex justify-content-center">
-                    <div class="card" style="width: 26rem;">
-                        <img src="./assets/img/${elementsToPrint[2]}" class="card-img-top">
-                        <div class="card-body text-center">
-                            <h5>${elementsToPrint[0]}</h5>
-                            <p class="card-text">${elementsToPrint[1]}</p>
-                        </div>
-                    </div>
-                    <!-- /.card  -->
-                </div>
-                <!-- /.col -->
+    cardPositionElement.innerHTML += 
     `
-  }
+      <div class="col d-flex justify-content-center">
+        <div class="card" style="width: 26rem;">
+            <img src="./assets/img/${elementsToPrint[2]}" class="card-img-top">
+            <div class="card-body text-center">
+                <h5>${elementsToPrint[0]}</h5>
+                <p class="card-text">${elementsToPrint[1]}</p>
+            </div>
+        </div>
+        <!-- /.card  -->
+      </div>
+      <!-- /.col -->
+    `
+}
 
+// form per aggiungere card
+
+// collego in btn add
+const btnAddElement = document.getElementById('add_user');
+// creo un event listener al click
+btnAddElement.addEventListener('click', function (event){
+  event.preventDefault();
+  //collego input 
+  const userNameElement = document.getElementById('user_name').value;
+  const userRoleElement = document.getElementById('user_role').value;
+  const userImgElement = document.getElementById('user_img').value;
+  // aggiungo la card con i nuovi dati
+  cardPositionElement.innerHTML += 
+  `
+    <div class="col d-flex justify-content-center">
+      <div class="card" style="width: 26rem;">
+          <img src="${userImgElement}" class="card-img-top">
+          <div class="card-body text-center">
+              <h5>${userNameElement}</h5>
+              <p class="card-text">${userRoleElement}</p>
+          </div>
+      </div>
+      <!-- /.card  -->
+    </div>
+    <!-- /.col -->
+  `
+});
